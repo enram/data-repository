c2391948e3746e1db8888020f02aaf778b5faf4e
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-19 17:10:26 +0100

    add coverage listing to ec2 routine

--- file_transfer/baltrad_to_s3.py
+++ file_transfer/baltrad_to_s3.py
@@ -6,6 +6,7 @@ import sys
 
 from creds import URL, LOGIN, PASSWORD
 from connectors import BaltradToS3
+from helper_functions import coverage_to_csv
 
 
 def main():
@@ -15,8 +16,12 @@ def main():
 
     # Execute the transfer
     btos.transfer(name_match="_vp_", overwrite=False, limit=None)
-
     btos.report(reset_file=False, transfertype="Baltrad to S3")
+
+    # Get the current coverage
+    coverage_count = btos.count_enram_coverage()
+    coverage_to_csv(coverage_count, filename='coverage.csv')
+    btos.upload_file("coverage.csv", "coverage.csv")
 
 
 if __name__ == "__main__":
