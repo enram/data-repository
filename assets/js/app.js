3679f3c6c441406329922dc2b8f0279a7bd3990e
Peter Desmet <peter.desmet.work@gmail.com>
2017-01-21 00:58:51 +0100

    Add calendar-heatmap
    Css and js copied from
    https://github.com/peterdesmet/calendar-heatmap/tree/09e24a8a9a0cf6dddd0
    6cf15121f64b1a5f15889/src

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,56 @@
-d3.csv("https://lw-enram.s3-eu-west-1.amazonaws.com/coverage.csv", function(error, csv) {
-    if (error) throw error;
+function getUrlParameter(name) {
+    // From https://davidwalsh.name/query-string-javascript
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
+var prefix = getUrlParameter('prefix').split("/");
+prefix.pop(); // remove last empty element
+
+if (prefix.length === 3) { // We are at country/radar/year level
     var expected_vp_files = 96;
+    var selector = "#heatmap";
+    var countryradar = prefix[0] + prefix[1]; // e.g. "deboo"
+    var year = prefix[2]; // e.g. "2016"
 
-    // process coverage data
-    var csv_parsed = csv.map(function(item) {
-        var vp_files = parseInt(item.vp_files); // cast to int
+    d3.csv("https://lw-enram.s3-eu-west-1.amazonaws.com/coverage.csv", function(error, csv) {
+        if (error) throw error;
 
-        return {
-            radar: item.countryradar,
-            date: new Date(item.date), // cast to date
-            vp_files: vp_files,
-            count: vp_files / expected_vp_files // percentage
-        };
+        // process coverage data
+        var csv_parsed = csv.map(function(item) {
+            return {
+                countryradar: item.countryradar,
+                date: new Date(item.date), // cast to date
+                count: parseInt(item.vp_files) // cast to int
+            };
+        });
+        var data = d3.nest()
+            .key(function(d) { return d.countryradar; }) // group data by radar
+            .key(function(d) { return moment(d.date).format("YYYY"); }) // group by year
+            .map(csv_parsed);
+
+        try {
+            var countryRadarYearData = data[countryradar][year]; // can be out of bounds (e.g. manually entered incorrect year in querystring)
+
+            // clear div
+            d3.select(selector).html("");
+
+            // render heatmap for czbrd in 2017
+            var heatmap = calendarHeatmap()
+                .data(data[countryradar][year])
+                .max("expected_vp_files") // expected vp files
+                .selector(selector)
+                .colorRange(["#ffffff", "#428bca"])
+                .tooltipEnabled("dflsmjf")
+                .tooltipUnit("vp file")
+                .startDate(moment(year + "-01-01").startOf('day').toDate()); // create Jan 1st date of that year
+            heatmap(); // render the chart
+        } catch (err) {
+            if (err.name === "TypeError") {
+                d3.select(selector).html("<p>😵  Oh noo, you selected something for which there are no data!</p>");
+            }
+        }     
     });
-    var data = d3.nest()
-        .key(function(d) { return d.radar; }) // group data by radar
-        .key(function(d) { return moment(d.date).format("YYYY"); }) // group by year
-        .map(csv_parsed);
-
-    // render heatmap for czbrd in 2017
-    var heatmap = calendarHeatmap()
-        .data(data["czbrd"]["2017"])
-        .selector("#heatmap")
-        .colorRange(["#f4f7f7", "#79a8a9"])
-        .tooltipEnabled(true)
-        .tooltipUnit("coverage");
-
-    heatmap(); // render the chart
-});
+}
