804f03d6411f1bec4a4b7e6e70f834823bc0883a
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-30 10:53:57 +0100

    update imports

--- file_transfer/__init__.py
+++ file_transfer/__init__.py
@@ -1,2 +1,4 @@
-from .helper_functions import *
-from .connectors import *
+from .helper_functions import (parse_filename, extract_month_updates,
+                               parse_coverage_month, coverage_to_csv)
+from .connectors import (GithubConnector, S3Connector, BaltradFTPConnector,
+                         BaltradToS3, LocalToS3)
