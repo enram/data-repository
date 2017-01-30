e9ad6ad1141bcd08d451ac5ab93d5c65ab5afb96
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-30 13:40:38 +0100

    change file writing to file handling

--- file_transfer/baltrad_to_s3.py
+++ file_transfer/baltrad_to_s3.py
@@ -6,11 +6,17 @@ import sys
 
 from creds import URL, LOGIN, PASSWORD
 from connectors import BaltradToS3
+from s3handlers import S3Handler
 from helper_functions import coverage_to_csv
 
 
 def main():
     """Run data transfer from Baltrad to S3"""
+
+    # ------------------
+    # DATA TRANSFER
+    # ------------------
+
     # Setup the connection of the Baltrad and S3
     btos = BaltradToS3(URL, LOGIN, PASSWORD, "lw-enram")
 
@@ -18,10 +24,23 @@ def main():
     btos.transfer(name_match="_vp_", overwrite=False, limit=None)
     btos.report(reset_file=False, transfertype="Baltrad to S3")
 
-    # Get the current coverage
-    coverage_count = btos.count_enram_coverage()
-    coverage_to_csv(coverage_count, filename='coverage.csv')
-    btos.upload_file("coverage.csv", "coverage.csv")
+    # ------------------
+    # UPDATE COVERAGE
+    # ------------------
+
+    # Connecto to S3 client
+    s3client = S3Handler("lw-enram")
+
+    # Rerun file list overview to extract the current coverage
+    coverage_count = s3client.count_enram_coverage(level='day')
+    with open("coverage.csv", 'w') as outfile:
+        coverage_to_csv(outfile, coverage_count)
+    s3client.upload_file("coverage.csv", "coverage.csv")
+
+    # ----------------------------
+    # UPDATE ZIP FILE AVAILABILITY
+    # ----------------------------
+    # Rerun ZIP handling to create files
 
 
 if __name__ == "__main__":
