f2feec2a9e04e8bb5e8a708188a34a9c5a491fe5
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-30 10:58:47 +0100

    refactor listing of files

--- file_transfer/connectors.py
+++ file_transfer/connectors.py
@@ -1,9 +1,7 @@
 
 import os
-from datetime import datetime
 from glob import glob
 from ftplib import FTP
-
 
 import boto3
 import botocore
@@ -14,10 +12,50 @@ from helper_functions import parse_filename
 
 class Connector:
     def download_file(self, file):
-        raise 'Not implemented'
+        """download a file to local drive"""
+        raise NotImplemented
 
-    def list_files(self, paths):
-        raise 'Not implemented'
+    def list_files(self, path=None, name_match="_vp_"):
+        """list the files within a given subfolder or relative path"""
+        raise NotImplemented
+
+
+class LocalConnector(Connector):
+
+    def __init__(self, filepath):
+        """Connector to handle local directory files, focusing on a specific
+        folder subset asa defined by the main filepath
+
+        :param filepath: main directory to work in
+        """
+        self.filepath = filepath
+
+    def download_file(self, file):
+        """"""
+        return NotImplemented
+
+    def list_files(self, path=None, name_match="_vp_", fullpaths=False):
+        """
+
+        :param path: relative defined to filepath
+        :param name_match:
+        :param fullpaths: bool define if the full path or only relatve path
+        should be returned
+        :return: iterator with the matching file names of the path folder and
+        subfolders
+        """
+        if path:
+            path_to_list = os.path.join(self.filepath, path, "**",
+                                        "*{}*".format(name_match))
+        else:
+            path_to_list = os.path.join(self.filepath, "**",
+                                        "*{}*".format(name_match))
+        for subpath in glob(path_to_list, recursive=True):
+            if name_match in subpath:
+                if fullpaths:
+                    yield path
+                else:
+                    yield os.path.split(path)[-1]
 
 
 class GithubConnector(Connector):
@@ -48,56 +86,70 @@ class GithubConnector(Connector):
                     'name': item['name']
                 }
 
-    def download_file(self, file):
-        response = requests.get(file['download_url'])
-        full_name = file['path']
+    def download_file(self, item):
+        """download a github file to the current working directory with the
+        same subfolders as represented in the repo
+
+        :param item: response dict from the github API
+        """
+        response = requests.get(item['download_url'])
+        full_name = item['path']
         os.makedirs(os.path.dirname(full_name), exist_ok=True)
         with open(full_name, 'wb') as w:
             w.write(response.content)
 
-    def list_files(self, paths):
+    def list_files(self, path=None):
         """
         Returns an iterator that allows you to iterate over all files (i.e.
-        the download link of each file) in the given paths.
-        :param paths: a list of paths on the remote location to list files from
+        the download link of each file) in the given path.
+
+        :param paths: a path on the remote location to find files
+
+        :return: yields a dictionary with the keys download_url, path and name
         """
-        for p in paths:
-            url = 'https://api.github.com/repos/{user}/{repo}/contents/{path}'.format(
-                user=self.repo_username,
-                repo=self.repo_name,
-                path=p
-            )
-            response = requests.get(url)
-            for f in self._parse_files_from_response(response):
-                yield f
+
+        url = 'https://api.github.com/repos/{user}/{repo}/contents/{path}'.format(
+            user=self.repo_username,
+            repo=self.repo_name,
+            path='' if not path else path
+        )
+        response = requests.get(url)
+        for f in self._parse_files_from_response(response):
+            yield f
 
 
 class S3Connector(Connector):
 
     def __init__(self, bucket_name=None):
         """
-        Initialize a S3Connector
+        Initialize a S3Connector by defining the bucket name. The credentials
+        to do so are implicitly derived. For local usage, save
+        a ~/.aws/credentials file with  your aws_access_key_id and
+        aws_secret_access_key.
+
         :param bucket_name: name of a bucket
         :type bucket_name: string
         """
         self.bucket_name = bucket_name
         self._connect_to_s3()
-        self.bucket = self._s3resource.Bucket(bucket_name)
+        self.bucket = self.s3resource.Bucket(bucket_name)
 
     def _connect_to_s3(self):
         """
-        Private method to connect to the S3 service
+        Private method to connect to the S3 service. Initaties both the
+        resource (s3resource) as well as the client (s3client)
         """
 
         # Create the resource
-        self._s3resource = boto3.resource('s3')
+        self.s3resource = boto3.resource('s3')
 
         # Get the S3 client from the resource
-        self.s3client = self._s3resource.meta.client
+        self.s3client = self.s3resource.meta.client
 
     def download_file(self, file):
         """
         Download a file with a given object_key from the bucket
+
         :param file: dict containing information about the file to be downloaded
         :return: nothing
         """
@@ -110,7 +162,8 @@ class S3Connector(Connector):
 
     def upload_file(self, filename, object_key):
         """
-        Upload a file to the bucket
+        Upload a (binary) file to the bucket
+
         :param filename: name of the file on the local system
         :param object_key: name of the file to be used in the bucket (note
         that subdirectories on S3 should be part of the object key)
@@ -131,19 +184,13 @@ class S3Connector(Connector):
         else:
             return input2check
 
-    def list_files(self):
-        """iterate over all bucjet files"""
-        for key in self.bucket.objects.all():
-            yield key.key.split("/")[-1]
-
-    def list_files_path(self, paths):
+    # TODO: add name_match to the list files
+    def list_files(self, path=None):
         """
         List all files in the bucket.
         """
 
-        paths = self._strchecklister(paths)
-
-        for path in paths:
+        if path:
             paginator = self.s3client.get_paginator('list_objects_v2')
             operation_parameters = {'Bucket': self.bucket_name,
                                     'Prefix': path
@@ -154,13 +201,16 @@ class S3Connector(Connector):
                     if item['Key'][-1] != '/':
                         # don't include directories themselves
                         yield item['Key']
+        else:
+            for key in self.bucket.objects.all():
+                yield key.key.split("/")[-1]
 
     def key_exists(self, file_key):
         """check if key is already inside a bucket, rel to path"""
 
         exists = False
         try:
-            self._s3resource.Object(self.bucket_name, file_key).load()
+            self.s3resource.Object(self.bucket_name, file_key).load()
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 exists = False
