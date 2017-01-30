d47060fdc15f54deb60f7146ecb400df0cfbf475
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-30 11:09:36 +0100

    update parse file test

--- file_transfer/tests.py
+++ file_transfer/tests.py
@@ -1,32 +1,40 @@
-from .helper_functions import *
-from .ext_service import *
+from .helper_functions import parse_filename
 
 # ==============================
 # Test helper functions
 # ==============================
 
+
 def test_parse_filename():
     example_names = [
         'bezav_pvol_20151009T0000Z.h5',
         'ukjer_pvol_20151010T0000Z.h5',
-        'dkste_vp_20151010T0000Z.h5'
+        'dkste_vp_20151010T0000Z.h5',
+        'bejab_vp_20161120235000.h5'
     ]
     results = [
-        {'radar_name': 'bezav', 'data_type': 'pvol', 'date_time': '201510090000'},
-        {'radar_name': 'ukjer', 'data_type': 'pvol', 'date_time': '201510100000'},
-        {'radar_name': 'dkste', 'data_type': 'vp', 'date_time': '201510100000'},
+        {'country': 'be', 'radar': 'zav', 'data_type': 'pvol', 'year': '2015',
+         'month': '10', 'day': '09', 'hour': '00', 'minute': '00'},
+        {'country': 'uk', 'radar': 'jet', 'data_type': 'pvol', 'year': '2015',
+         'month': '10', 'day': '10', 'hour': '00', 'minute': '00'},
+        {'country': 'dk', 'radar': 'ste', 'data_type': 'vp', 'year': '2015',
+         'month': '10', 'day': '10', 'hour': '00', 'minute': '00'},
+        {'country': 'be', 'radar': 'jab', 'data_type': 'vp', 'year': '2016',
+         'month': '11', 'day': '20', 'hour': '23', 'minute': '50'}
     ]
+
     for i, name in enumerate(example_names):
         expected_result = results[i]
         result = parse_filename(name)
-        for key in ['radar_name', 'data_type', 'date_time']:
+        for key in ['country', 'data_type', 'day', 'hour', 'minute',
+                    'month', 'radar', 'year']:
             assert result[key] == expected_result[key]
-
 
 
 # ==============================
 # Test connectors with external services
 # ==============================
+
 
 def test_list_files_from_github():
     """
