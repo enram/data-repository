48ff585da5f499abeedb73d1e131a6d488644a05
stijnvanhoey <stijnvanhoey@gmail.com>
2017-04-12 13:28:10 +0200

    Fix namespace bug of FTPconnector

--- file_transfer/datamover/__init__.py
+++ file_transfer/datamover/__init__.py
@@ -1,6 +1,6 @@
 
 from .connectors import (GithubConnector, S3Connector,
-                         BaltradFTPConnector, LocalConnector)
+                         FTPConnector, LocalConnector)
 from .transporters import (BaltradToS3, LocalToS3)
 from .s3enram import S3EnramHandler
 from .utils import (parse_filename, extract_month_updates,
