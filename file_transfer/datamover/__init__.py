0e044d1ad8b6fb2b0ac2126bb0fccfa05de9da14
stijnvanhoey <stijnvanhoey@gmail.com>
2018-01-15 15:39:01 +0100

    Add csv handling to module

--- file_transfer/datamover/__init__.py
+++ file_transfer/datamover/__init__.py
@@ -4,4 +4,5 @@ from .connectors import (GithubConnector, S3Connector,
 from .transporters import (BaltradToS3, LocalToS3)
 from .s3enram import S3EnramHandler
 from .utils import (parse_filename, extract_month_updates,
-                    parse_coverage_month, coverage_to_csv)
+                    parse_coverage_month, coverage_to_csv,
+                    most_recent_to_csv)
