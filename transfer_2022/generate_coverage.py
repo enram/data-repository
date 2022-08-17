4b11f838b2f569daf96bdb470a0884000c810a20
Nicolas Noé <nicolas.noe@inbo.be>
2022-08-17 16:48:28 +0200

    First iteration of generate_coverage is feature-complete

--- transfer_2022/generate_coverage.py
+++ transfer_2022/generate_coverage.py
@@ -1,5 +1,8 @@
 # Script that parse the whole aloft bucket and generate a coverage.csv file
 # After discussion with Peter, we decided to keep a simple file count per directory (all listed in a global CSV file, one row per "directory")
+import csv
+import os
+import tempfile
 from collections import defaultdict
 from configparser import ConfigParser
 from pathlib import PurePath
@@ -23,12 +26,30 @@ def main():
     counter = defaultdict(int)
 
     print("Looping over files to count")
-    for e in s3list(bucket, "", recursive=True, list_dirs=True, list_objs=True):
+    for i, e in enumerate(s3list(bucket, "", recursive=True, list_dirs=True, list_objs=True)):
         dir = PurePath(e.key).parent
         counter[str(dir)] += 1
         print(".", end="")
 
+        if (i >= 10000):
+            break
+
+
     print("Done, will now generate coverage.csv")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmp_coverage_path = os.path.join(tmpdirname, "coverage.csv")
+        with open(tmp_coverage_path, 'w') as csvfile:
+            fieldnames = ['directory', 'file_count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+
+            for k, v in counter.items():
+                writer.writerow({'directory': k, 'file_count': v})
+
+        print("File generated, will now upload it to S3")
+        bucket.upload_file(tmp_coverage_path, 'coverage.csv')
+        print("Done")
 
 
 if __name__ == "__main__":
